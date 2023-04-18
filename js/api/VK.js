a257e6690d8cdf75f8a5843dfd91e2590742644e
Liaksej <tarusov.liaksej@gmail.com>
2023-04-18 21:16:52 +0200

    Add main functionality in VK.js
    But this is a raw version without connetcing all element between eachother.

--- js/api/VK.js
+++ js/api/VK.js
@@ -5,22 +5,43 @@
  * Имеет свойства ACCESS_TOKEN и lastCallback
  * */
 class VK {
-
-  static ACCESS_TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008';
-  static lastCallback;
+  static ACCESS_TOKEN =
+    "052ec054052ec054052ec0543f063d62ea0052e052ec0546110b1c17168e0e97c2f4d30";
+  static lastCallback = Object;
 
   /**
    * Получает изображения
    * */
-  static get(id = '', callback){
-
+  static get(id = "", callback) {
+    let script = document.createElement("SCRIPT");
+    script.src = `https://api.vk.com/method/photos.get?owner_id=${id}&album_id=profile&photo_sizes=1&count=1000&access_token=${this.ACCESS_TOKEN}&v=5.131&callback=callbackFn`;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    callbackFn = function (result) {
+      callback(result);
+    };
   }
 
   /**
    * Передаётся в запрос VK API для обработки ответа.
    * Является обработчиком ответа от сервера.
    */
-  static processData(result){
-
+  static processData(result) {
+    document.head.querySelector("script").remove();
+    if (!result) {
+      alert(
+        "Запрос данных в VK не прошел. Проверьте подключение к Интернету и попробуйте еще раз."
+      );
+      return;
+    }
+    const photoArray = result.response.items;
+    const photoMaxSize = [];
+    for (const photo of photoArray) {
+      photoMaxSize.push(photo.sizes.at(-1).url);
+    }
+    VK.lastCallback = photoMaxSize;
+    console.log(VK.lastCallback);
   }
 }
+
+let callbackFn;
+VK.get("7451160", VK.processData);
