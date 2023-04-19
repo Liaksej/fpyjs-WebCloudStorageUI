b483f9db52e866f22252cae1a17916190569505a
Liaksej <tarusov.liaksej@gmail.com>
2023-04-19 15:24:25 +0200

    Changes the transfer of the callback response via JSONP to the class
    The change saves us from having to create the callback in a global environment. We create it directly in the class.

--- js/api/VK.js
+++ js/api/VK.js
@@ -8,19 +8,19 @@ class VK {
   static ACCESS_TOKEN =
     "052ec054052ec054052ec0543f063d62ea0052e052ec0546110b1c17168e0e97c2f4d30";
   static lastCallback = {
-    callback: VK.get("7451160", VK.processData),
+    // callback: VK.get("7451160"),
+    callbackFn: function (result) {
+      VK.processData(result);
+    },
   };
 
   /**
    * Получает изображения
    * */
-  static get(id = "", callback) {
+  static get(id = "7451160") {
     let script = document.createElement("SCRIPT");
-    script.src = `https://api.vk.com/method/photos.get?owner_id=${id}&album_id=profile&photo_sizes=1&count=1000&access_token=${this.ACCESS_TOKEN}&v=5.131&callback=callbackFn`;
+    script.src = `https://api.vk.com/method/photos.get?owner_id=${id}&album_id=profile&photo_sizes=1&count=1000&access_token=${this.ACCESS_TOKEN}&v=5.131&callback=VK.lastCallback.callbackFn`;
     document.getElementsByTagName("head")[0].appendChild(script);
-    window.callbackFn = function (result) {
-      callback(result);
-    };
   }
 
   /**
@@ -41,7 +41,7 @@ class VK {
       photoMaxSize.push(photo.sizes.at(-1).url);
     }
     VK.lastCallback.listFromCallback = photoMaxSize;
-    VK.lastCallback.callback = () => {};
-    VK.lastCallback.allert = console.log(VK.lastCallback.listFromCallback);
+    VK.lastCallback.callbackFn = () => {};
+    // VK.lastCallback.allert = console.log(VK.lastCallback.listFromCallback);
   }
 }
