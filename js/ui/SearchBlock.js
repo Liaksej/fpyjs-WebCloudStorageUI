c97c2cced731392cde1708f5f5290903183b3214
Liaksej <tarusov.liaksej@gmail.com>
2023-04-19 17:04:25 +0200

    Add main functionality to SearchBlock.js according to the task

--- js/ui/SearchBlock.js
+++ js/ui/SearchBlock.js
@@ -3,8 +3,9 @@
  * Используется для взаимодействием со строкой ввода и поиска изображений
  * */
 class SearchBlock {
-  constructor( element ) {
-
+  constructor(element) {
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -12,8 +13,39 @@ class SearchBlock {
    * Клик по кнопкам выполняет запрос на получение изображений и отрисовывает их,
    * только клик по кнопке "Заменить" перед отрисовкой очищает все отрисованные ранее изображения
    */
-  registerEvents(){
+  registerEvents() {
+    document
+      .querySelector(".search-block")
+      .addEventListener("click", findImages);
 
+    function findImages(event) {
+      const input = document
+        .querySelector(".search-block")
+        .getElementsByTagName("input")[0];
+      if (input.value.trim()) {
+        if (event.target === document.querySelector(".replace")) {
+          document.querySelector(".images-list").remove();
+          VK.get(input.value);
+          setTimeout(() => {
+            let photoList = VK.lastCallback.listFromCallback;
+            for (const photo of photoList) {
+              console.log(photo);
+              App.imageViewer;
+            }
+          }, 2000);
+          return;
+        }
+        if (event.target === document.querySelector(".add")) {
+          VK.get(input.value);
+          setTimeout(() => {
+            let photoList = VK.lastCallback.listFromCallback;
+            for (const photo of photoList) {
+              console.log(photo);
+              App.imageViewer;
+            }
+          }, 2000);
+        }
+      }
+    }
   }
-
 }
