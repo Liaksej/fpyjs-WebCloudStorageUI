b24fb947446a037fe0ac005444a5558928f54006
Liaksej <tarusov.liaksej@gmail.com>
2023-04-21 23:02:47 +0200

    Add main functionality to createRequest.js

--- js/api/createRequest.js
+++ js/api/createRequest.js
@@ -2,5 +2,14 @@
  * Основная функция для совершения запросов по Yandex API.
  * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
 
+  try {
+    xhr.open(options.method, Yandex.HOST);
+    xhr.responseType = "json";
+    xhr.setRequestHeader(options.headers);
+    xhr.send(data);
+  } catch (err) {
+    console.error(err);
+  }
 };
