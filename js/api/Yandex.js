4a4df3155db7d7949feee260bd9fca86a1610666
Liaksej <tarusov.liaksej@gmail.com>
2023-04-21 23:03:22 +0200

    Add partial functionality to Yandex.js

--- js/api/Yandex.js
+++ js/api/Yandex.js
@@ -4,40 +4,56 @@
  * Имеет свойство HOST
  * */
 class Yandex {
-  static HOST = 'https://cloud-api.yandex.net/v1/disk';
+  static HOST = "https://cloud-api.yandex.net/v1/disk";
 
   /**
    * Метод формирования и сохранения токена для Yandex API
    */
-  static getToken(){
-
+  static getToken() {
+    if (!localStorage.hasOwnProperty("yaToken")) {
+      localStorage.setItem(
+        "yaToken",
+        prompt("Введите OAUth-токен от Яндекс.Диска")
+      );
+    }
   }
 
   /**
    * Метод загрузки файла в облако
    */
-  static uploadFile(path, url, callback){
-
-  }
+  static uploadFile(path, url, callback) {}
 
   /**
    * Метод удаления файла из облака
    */
-  static removeFile(path, callback){
-
+  static removeFile(path, callback) {
+    createRequest({
+      method: "DELETE",
+      path: "/resources/upload",
+      data: { path: `${path}` },
+      headers: {
+        Authorization: `${localStorage.getItem("yaToken")}`,
+      },
+    });
   }
 
   /**
    * Метод получения всех загруженных файлов в облаке
    */
-  static getUploadedFiles(callback){
-
+  static getUploadedFiles(callback) {
+    createRequest({
+      method: "GET",
+      data: "/resources/files",
+      headers: {
+        Authorization: `${localStorage.getItem("yaToken")}`,
+      },
+    });
   }
 
   /**
    * Метод скачивания файлов
    */
-  static downloadFileByUrl(url){
-
-  }
+  static downloadFileByUrl(url) {}
 }
+
+Yandex.getToken();
